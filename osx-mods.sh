e7e0d987d9dbcfcf978f0afc51f5262936f8e20d
Sujith G <sujith3g@gmail.com>
2017-01-29 22:16:37 +0530

    Add default screenshot location to osx-mods.sh

--- osx-mods.sh
+++ osx-mods.sh
@@ -1,4 +1,10 @@
+#! /bin/bash
+
+# Scripts for modify default configs of OSX
 
 ## Show hidden files in the finder
 defaults write com.apple.finder AppleShowAllFiles YES
 killall Finder
+## save screenshots to ~/Desktop/screenshots/
+defaults write com.apple.screencapture location ~/Desktop/screenshots/
+killall SystemUIServer
